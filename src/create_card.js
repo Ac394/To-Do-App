6d6f97f43054b6c6cb34497360fb305b60da4276
Ac394 <panos194@hotmail.com>
2023-09-21 16:30:39 +0300

    Project structure

---
+++ src/create_card.js
@@ -0,0 +1,94 @@
+export default function newCard(task) {
+    const newEl = (e) => {
+      return document.createElement(`${e}`)
+    }
+  
+    const cardsContainer = document.querySelector(".cards-container");
+  
+    const cardWrap = newEl("a");
+    cardWrap.classList.add("card-wrapper");
+  
+    const descrWrap = newEl("div");
+    descrWrap.classList.add("description-wrapper");
+  
+    const txtArea = newEl("textarea");
+    txtArea.classList.add("txta");
+    txtArea.placeholder = "Add a description"
+    txtArea.addEventListener("change", () => {
+      task.description = txtArea.value;
+      console.log(task.description);
+      console.log(task);
+    })
+  
+    const cardInf = newEl("div");
+    cardInf.classList.add("card-info");
+  
+    const infoWrap = newEl("div");
+    infoWrap.classList.add("info-wrapper");
+  
+    const dropdown = newEl("div");
+    dropdown.classList.add("dropdown")
+  
+    const dropBtn = newEl("button");
+    dropBtn.classList.add("dropbtn");
+    dropBtn.addEventListener("click", () => {
+      dropContent.classList.toggle("show");
+    })
+  
+    const dropContent = newEl("div");
+    dropContent.classList.add("dropdown-content")
+    dropContent.setAttribute("id", "myDropdown");
+  
+    const lowBtn = newEl("button");
+    lowBtn.classList.add("low-pr");
+  
+    const midBtn = newEl("button");
+    midBtn.classList.add("mid-pr");
+  
+    const highBtn = newEl("button");
+    highBtn.classList.add("high-pr");
+  
+    const dateInput = newEl("input");
+    dateInput.classList.add("date-input");
+    dateInput.setAttribute("type", "date");
+    dateInput.setAttribute("id", "datepicker");
+  
+    const dateLabel = newEl("label");
+    dateLabel.classList.add("date");
+    dateLabel.innerHTML = "Apr 23"
+    dateLabel.setAttribute("for", "datepicker");
+    dateLabel.addEventListener("click", () => { dateInput.showPicker() });
+  
+    const checkDiv = newEl("div");
+    checkDiv.classList.add("checkDiv")
+  
+    const delIcon = newEl("span");
+    delIcon.innerHTML = "delete"
+    delIcon.classList.add("material-symbols-outlined");
+    delIcon.addEventListener("click", () => {
+      cardWrap.remove();
+    })
+  
+    const checkbox = newEl("input");
+    checkbox.classList.add("checkbox")
+    checkbox.setAttribute("id", "checkbox");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.setAttribute("name", "checkbox");
+  
+    descrWrap.appendChild(txtArea);
+    dropContent.appendChild(lowBtn, midBtn, highBtn);
+    dropContent.appendChild(midBtn);
+    dropContent.appendChild(highBtn);
+    dropdown.appendChild(dropBtn, dropContent);
+    dropdown.appendChild(dropContent);
+    infoWrap.appendChild(dropdown);
+    infoWrap.appendChild(dateInput);
+    infoWrap.appendChild(dateLabel);
+    checkDiv.appendChild(delIcon);
+    checkDiv.appendChild(checkbox);
+    cardInf.appendChild(infoWrap);
+    cardInf.appendChild(checkDiv);
+    cardWrap.appendChild(descrWrap);
+    cardWrap.appendChild(cardInf);
+    cardsContainer.appendChild(cardWrap);
+  }
---
+++ src/index.js
@@ -0,0 +1,121 @@
+import { compareAsc, format } from 'date-fns'
+import './style.css';
+import textareaWrap from "/src/textarea.js";
+import newCard from "/src/create_card.js";
+
+// format(new Date(2014, 1, 11), 'yyyy-MM-dd')
+
+const dates = [
+    new Date(1995, 6, 2),
+    new Date(1987, 1, 11),
+    new Date(1989, 6, 10),
+  ]
+
+  console.log(format(dates[0], 'yyyy-MM-dd'));
+
+  textareaWrap();
+
+const tasks = [];
+
+class Task {
+  constructor(description, dueDate, priority, check, project) {
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.check = check;
+    this.project = project;
+  }
+
+  // get description() {
+  //   return this.description;
+  // }
+
+  // set description(updateDescription) {
+  //   this.description = updateDescription;
+  // }
+
+  // get dueDate() {
+  //   return this.dueDate;
+  // }
+
+  // set dueDate(updateDate) {
+  //   this.dueDate = updateDate;
+  // }
+
+  // get priority() {
+  //   return this.priority;
+  // }
+
+  // set priority(updatePriority) {
+  //   this.priority = updatePriority;
+  // }
+
+  // get check() {
+  //   return this.check;
+  // }
+
+  // set check(updateCheck) {
+  //   this.check = updateCheck;
+  // }
+
+  // get project() {
+  //   return this.project;
+  // }
+
+  // set project(updateProject) {
+  //   this.project = updateProject;
+  // }
+}
+
+let fck = new Task;
+newCard(fck);
+
+// let task1 = new Task("Clean up the dishes", "15 Jul 24", "low", false, "Default")
+
+// tasks.push(task1);
+
+const createCard = (description = "", dueDate = "Apr 23", priority
+  = "low", check = "false", project) => {
+  const task = new Task(description);
+}
+
+// Date Label Update
+const dateInput = document.getElementById("datepicker");
+const dateLabel = document.getElementsByClassName("date");
+const dateChange = () => { dateLabel[0].innerHTML = dateInput.value };
+
+dateInput.addEventListener("change", dateChange);
+
+// Event listener for Add Task button
+const addTask = document.querySelector(".add-task");
+
+addTask.addEventListener("click", newCard);
+
+
+///////////////////////////////////////
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+
+const myDropdown = document.querySelector("#myDropdown");
+function dropdown() {
+  myDropdown.classList.toggle("show");
+  console.log("fck");
+}
+
+const dropbtn = document.querySelector(".dropbtn");
+dropbtn.addEventListener("click", dropdown);
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
